ef31f869e548969ba24b3f897aac8e062ddb36af
jcari-rgb <582119jc@gmail.com>
2021-03-23 10:58:03 -0400

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos, Isnt that Entropy

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -26,11 +26,12 @@ $(() => {
     $('ul').append('<li class="secret">invisibility Cloak</li>');
     $('ul').append('<li class="secret">magic map</li>');
     $('ul').append('<li class="secret">time turner</li>');
-    $('ul').append('<li class="cat">leash</li>');
+    $('ul').append('<li class="seaUnicorn">leash</li>');
     $('ul').append("<li>Bertie Bott's Every Flavor [Jelly] Beans</li>");
-    $('#container').append($ul);
     //======================== Year 4
+    $('#container').append('<h5>Winter/Spring/Summer 2021</h5>'); // Are we really doing an one liner here...                                             Yup....                                   Woah....                        I promise it's kind of readable...                                Please don't fire me I have a wife (not yet, but in July!) and three kids (it's actually one cactus)
+    $('#container').append('<table><tr><th>Day</th><th>Classes</th><tr><td>Monday</td><td>None</td></tr><tr><td>Tuesday</td><td>SEI302</td></tr><tr><td>Wednesday</td><td>SEI302</td></tr><tr><td>Thursday</td><td>SEI302 Outcomes</td></tr><tr><td>Friday</td><td>None</td></tr><tr><td>Saturday</td><td>SEI302</td></tr><tr><td>Sunday</td><td>None</td></tr>')
+    // in all seriousness, im sorry I am aware that this is prob not good practice, I'm sorry!
 
-
-
+    //======================== Year 5
 })
